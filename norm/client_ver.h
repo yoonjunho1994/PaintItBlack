b404ec7e29b4fd4be6d8c3e27d0d674549e9a8fa
Norman Ziebal <Norman.Ziebal@web.de>
2019-04-10 23:49:28 +0200

    small cleanup

--- norm/client_ver.h
+++ norm/client_ver.h
@@ -2,15 +2,15 @@
 #include "stdafx.h"
 #include <stdio.h>
 
-#define CLIENT_VER 20150000 /* needs to fit the client date of the exe. */
-//#define CLIENT_VER_RE 20180621
+//#define CLIENT_VER 20180621 /* needs to fit the client date of the exe. */
+#define CLIENT_VER_RE 20180621
 //#define DEBUG	/* requires a debug-server listening on localhost:1337 */
 
 /* simple macro for some debug output (used with the debug server) */
 #define CHECK(buf, errno_) (errno_ == 0) ? sprintf_s(buf, "Success at Line %s:%d", __FILE__ ,__LINE__) : \
 	sprintf_s(buf, "Error %d at %s:%d", errno_, __FILE__ ,__LINE__)
 
-/* disable mods by undefining */
+/* disable mods by undefining OUTDATED*/
 //#define COMENC	/* requires src edit to enable "encryption" on server side. */
 #define CHAT_INPUT
 
@@ -21,5 +21,9 @@
 #if CLIENT_VER == 20180621
 #define DLL_VER "RO_2018-06-21Ragexe"
 #elif CLIENT_VER == 20150000
-#define DLL_VER "Einherjar"
+#define DLL_VER "RO_2015-05-13Ragexe"
+#elif CLIENT_VER == 20180620
+#define DLL_VER "RO_2018-06-20#2Ragexe"
+#elif CLIENT_VER_RE == 20180621
+#define DLL_VER "RO_2018-06-21RagexeRE"
 #endif
