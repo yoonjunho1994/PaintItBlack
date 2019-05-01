2599ff89bbd8feb654f026fc2830d4da600266cc
Norman Ziebal <Norman.Ziebal@web.de>
2019-05-02 01:56:35 +0200

    follow up

--- norm/client_ver.h
+++ norm/client_ver.h
@@ -2,7 +2,7 @@
 #include "stdafx.h"
 #include <stdio.h>
 
-//#define CLIENT_VER 20180621/* needs to fit the client date of the exe. */
+//#define CLIENT_VER 20150000/* needs to fit the client date of the exe. */
 //#define CLIENT_VER_RE 20180621
 //#define DEBUG	/* requires a debug-server listening on localhost:1337 */
 
